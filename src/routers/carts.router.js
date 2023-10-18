34f9519e05717e5777fa541f4a16259c0b4894c6
Antonella Gatti <antonellagatti@MacBook-Air-de-Antonella.local>
2023-10-18 02:34:24 -0300

    Correccion carts router

--- src/routers/carts.router.js
+++ src/routers/carts.router.js
@@ -7,10 +7,6 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
-// funciona con ruta ABSOLUTA pero no con ruta relativa 
-// const ruta = '/Users/antonellagatti/Desktop/Antu/CODERHOUSE/BACKEND/PreEntregas/PreEntrega1Gatti/src/carrito.json'
 const ruta = path.join(__dirname, '../carrito.json');
 
 const router = Router();
@@ -20,7 +16,7 @@ router.post('/carts', async (req, res) => {
     const body = req.body;
     const carts = await getJSONFromFile(ruta);
     const { cid, products } = body;
-    
+
     const newCart = {
         id: uuidv4(),
         products: [],
@@ -39,67 +35,86 @@ router.get('/carts/:cid', async (req, res) => {
 
     let existingCart = carts.find(cart => cart.id === cid)
     if (!existingCart) {
-        
+
         console.error('Sorry! We did not found that id');
         res.status(404).json({ message: 'Cart not found' });
     }
     res.json(existingCart)
-    
+
 })
 
 // funcion para buscar carrito
-function getCartbyId(cid) {
-    try {
-        const carts = getJSONFromFile(ruta);
-        let existingCart = carts.find(cart => cart.id === cid);
-        if (!existingCart) {
-            console.error('Sorry! We did not find that id');
-            return null;
-        }
-        return existingCart;
-    } catch (error) {
-        console.error('Error getting cart by ID:', error);
-        throw error;
-    }
+function getCartbyId(carts, cid) {
+    return carts.find(cart => cart.id === cid);
 }
 
-
-
-
-// funcion para buscar el producto x Id
-
-
-
+// Agregar producto al carrito o modificar cantidades
 router.post('/:cid/product/:pid', async (req, res) => {
     try {
         const cid = req.params.cid
         const productId = req.params.pid;
         const carts = await getJSONFromFile(ruta);
-        const cart = getCartbyId(cid)
+        const cart = getCartbyId(carts, cid)
 
- if (!cart) {
-        return res.status(404).json({ error: 'El carrito no existe' });
-    }else {
-        const existingCartItem = cart.products.find(item => item.product === productId);
-
-        if (!existingCartItem) {
-            cart.products.push({ 
-                product: productId, 
-                quantity: 1 })
+        if (!cart) {
+            return res.status(404).json({ error: 'The cart does not exists' });
         } else {
-            existingCartItem.quantity += 1;
+            const existingCartItem = cart.products.find(item => item.product === productId);
+            const quantityToAdd = req.body.quantity || 1;
+
+            if (!existingCartItem) {
+                cart.products.push({
+                    product: productId,
+                    quantity: quantityToAdd,
+                });
+            } else {
+                existingCartItem.quantity += quantityToAdd;
+            }
+            await saveJSONFromFile(ruta, carts);
+            return res
+                .status(200)
+                .json({ message: 'Product added to cart' });
         }
-        await saveJSONFromFile(ruta, carts);
-        return res
-        .status(200)
-        .json({ message: 'Producto agregado al carrito' });
-    }
-    } catch {
+    } catch (error){
         console.error('Error adding product to cart:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error });
     }
 
 })
+
+//Borrar productos del carrito, especificando cantidad.
+router.delete('/:cid/product/:pid', async (req, res) => {
+    try {
+        const cid = req.params.cid;
+        const productId = req.params.pid;
+        const quantityToRemove = req.body.quantity || 1; 
+        const carts = await getJSONFromFile(ruta);
+        const cart = getCartbyId(carts, cid);
+
+        if (!cart) {
+            return res.status(404).json({ error: 'The cart does not exists' });
+        } else {
+            const existingCartItem = cart.products.find(item => item.product === productId);
+
+            if (!existingCartItem) {
+                return res.status(404).json({ error: 'The product does not exist in the cart' });
+            } else {
+                if (existingCartItem.quantity <= quantityToRemove) {
+                    const index = cart.products.indexOf(existingCartItem);
+                    cart.products.splice(index, 1);
+                } else {
+                    existingCartItem.quantity -= quantityToRemove;
+                }
+
+                await saveJSONFromFile(ruta, carts);
+                return res.status(200).json({ message: 'Product quantity removed from cart' });
+            }
+        }
+    } catch (error) {
+        console.error('Error deleting product from cart:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 
 
@@ -129,3 +144,4 @@ async function saveJSONFromFile(path, data) {
 }
 
 export default router;
+
